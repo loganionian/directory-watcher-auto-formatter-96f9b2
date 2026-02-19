ca2da2517a6cf945cde7649588daf4c3e58974ae
loganionian <103104454+loganionian@users.noreply.github.com>
2026-02-19 19:53:40 +0800

    Add src/directory_watcher_auto_formatter/__init__.py

---
+++ src/directory_watcher_auto_formatter/__init__.py
@@ -0,0 +1,6 @@
+"""
+directory-watcher-auto-formatter - A CLI tool that watches a directory and automatically formats Python files on save using Black and isort.
+"""
+from .core import start_watching  # Export main functionality
+
+__version__ = "0.1.0"
